b5886610a9276babfed3d08b995dc1863b9783ff
Bartosz <bartosz.cylwik94@gmail.com>
2021-03-03 14:40:41 +0100

    add content to error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 const ErrorPage = () => {
   return (
-    <div>This is an error page</div>
+    <div className='error-page'>
+      <h1>Podana strona nie istnieje</h1>
+      <div className="item-buy">
+        <button><Link to='/'>Idź do strony głównej</Link></button>
+      </div>
+    </div>
   );
 }
 
